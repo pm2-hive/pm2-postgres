51d3213e54d105c318425d6356ef47a9f13ccc07
didil <haritahadil@yahoo.fr>
2015-10-03 15:39:57 +0100

    init

---
+++ README.md
@@ -0,0 +1,53 @@
+# pm2-postgres
+PostgreSQL module for Keymetrics
+
+![pm2-postgres screenshot](https://raw.githubusercontent.com/pm2-hive/pm2-postgres/master/pm2-postgres.jpg)
+
+## Description
+
+PM2 module to monitor key PostgreSQL server metrics:
+
+* 
+* 
+* 
+*
+
+## Requirements
+
+This module requires a PostgreSQL install (tested against v9.4).
+
+## Install
+
+```bash
+$ npm install pm2 -g
+
+$ pm2 install pm2-postgres
+```
+
+## Config
+
+The default connection details are :    
+"hostname": "localhost"  
+"port": 5432  
+"username": "guest"  
+"password": "guest"  
+"database": "postgres"  
+
+To modify the config values you can use the commands:
+```bash
+$ pm2 set pm2-postgres:hostname localhost
+$ pm2 set pm2-postgres:port 5432
+$ pm2 set pm2-postgres:username guest
+$ pm2 set pm2-postgres:password guest
+$ pm2 set pm2-postgres:database postgres
+```
+
+## Uninstall
+
+```bash
+$ pm2 uninstall pm2-postgres
+```
+
+# License
+
+MIT
---
+++ app.js
@@ -0,0 +1,49 @@
+var pmx = require('pmx');
+var pgClientFactory = require('./lib/clientFactory.js');
+var pgStats = require('./lib/stats.js');
+var pgActions = require('./lib/actions.js');
+
+pmx.initModule({
+
+  pid: pmx.resolvePidPaths(['/var/run/postgresql/9.4-main.pid', '/var/run/postgresql/9.3-main.pid']),
+
+  // Options related to the display style on Keymetrics
+  widget: {
+
+    // Logo displayed
+    logo: 'http://www.inquidia.com/sites/default/files/postgresql_logo%5B1%5D.png',
+
+    // Module colors
+    // 0 = main element
+    // 1 = secondary
+    // 2 = main border
+    // 3 = secondary border
+    theme: ['#141A1F', '#222222', '#FC6400', '#807C7C'],
+
+    // Section to show / hide
+    el: {
+      probes: true,
+      actions: true
+    },
+
+    // Main block to show / hide
+    block: {
+      actions: true,
+      issues: true,
+      meta: true,
+
+      // Custom metrics to put in BIG
+      main_probes: ['Tables', 'Indexes']
+    }
+
+  }
+
+}, function (err, conf) {
+  var pgClient = pgClientFactory.build(conf);
+
+  // Init metrics refresh loop
+  pgStats.init(pgClient);
+
+  // Init actions
+  pgActions.init(pgClient);
+});
